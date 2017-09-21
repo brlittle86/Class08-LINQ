3f8dcd16c709777ffc2edf52e3ab4be07d533213
Brandon Little <brlittle86@gmail.com>
2017-09-21 11:06:56 -0700

    did all the things: used Newtonsoft.Json JSON deserialization to bring in JSON data, then built a series of LINQ queries to sort through the data and display desired information

--- lab08-brandon/LINQLab/LINQLab/Feature.cs
+++ lab08-brandon/LINQLab/LINQLab/Feature.cs
@@ -5,9 +5,33 @@ using System.Text;
 
 namespace LINQLab
 {
-    class Feature
+    public class Geometry
     {
-        [JsonProperty("neighborhood")]
+        public string Type { get; set; }
+        public List<double> Coordinates { get; set; }
+    }
+
+    public class Properties
+    {
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Address { get; set; }
+        public string Borough { get; set; }
         public string Neighborhood { get; set; }
+        public string County { get; set; }
+    }
+
+    public class Feature
+    {
+        public string Type { get; set; }
+        public Geometry Geometry { get; set; }
+        public Properties Properties { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string Type { get; set; }
+        public List<Feature> Features { get; set; }
     }
 }
--- lab08-brandon/LINQLab/LINQLab/Program.cs
+++ lab08-brandon/LINQLab/LINQLab/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -8,16 +10,48 @@ namespace LINQLab
     {
         static void Main(string[] args)
         {
-            string deserializedJSON = GetJSONString();
-        }
-
-        static string GetJSONString()
-        {
+            string json;
             using (StreamReader sr = File.OpenText("data.json"))
             {
-                string[] json = File.ReadAllLines("data.json");
-                return json[0];
+                json = File.ReadAllText("data.json");
             }
+            RootObject featuresCollection = JsonConvert.DeserializeObject<RootObject>(json);
+
+            Console.WriteLine("Question 1: Display all neighborhoods.");
+            var allFeatures = from o in featuresCollection.Features
+                              where o.Properties.Neighborhood != null
+                              select o;
+
+            foreach (Feature item in allFeatures)
+            {
+                Console.WriteLine(item.Properties.Neighborhood);
+            }
+            Console.WriteLine();
+            
+            Console.WriteLine("Question 2: Remove blank Neighborhood entries.");
+            var allNeighborhoods = allFeatures.Where(j => j.Properties.Neighborhood != "");
+            foreach (Feature item in allNeighborhoods)
+            {
+                Console.WriteLine(item.Properties.Neighborhood);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Question 3: Remove duplicate Neighborhoods.");
+            var uniqueNeighborhoods = allNeighborhoods.GroupBy(p => p.Properties.Neighborhood).Select(m => m.First());
+            foreach (Feature item in uniqueNeighborhoods)
+            {
+                Console.WriteLine(item.Properties.Neighborhood);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Question 4: Do it all in one line.");
+            var doItAll = featuresCollection.Features.Where(j => j.Properties.Neighborhood != "").GroupBy(p => p.Properties.Neighborhood).Select(m => m.First());
+            foreach (Feature item in doItAll)
+            {
+                Console.WriteLine(item.Properties.Neighborhood);
+            }
+            Console.Read();
+            
         }
     }
 }
