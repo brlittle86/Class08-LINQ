2c7383b1aa5c6b81d4809f1ea62865db928cf695
Brandon Little <brlittle86@gmail.com>
2017-09-21 09:00:31 -0700

    Feature class set up with property for neighborhood

--- lab08-brandon/LINQLab/LINQLab/Feature.cs
+++ lab08-brandon/LINQLab/LINQLab/Feature.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,7 @@ namespace LINQLab
 {
     class Feature
     {
+        [JsonProperty("neighborhood")]
+        public string Neighborhood { get; set; }
     }
 }
